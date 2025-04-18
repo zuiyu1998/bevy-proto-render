ae19188e1fc495d1dce6e38db58c73fde1cf9dda
zuiyu1998 <1542844298@qq.com>
2025-04-18 23:43:16 +0800

    feat: Add ResourceNode

--- src/frame_graph/mod.rs
+++ src/frame_graph/mod.rs
@@ -1,9 +1,11 @@
 pub mod pass_node;
 pub mod render_context;
 pub mod resource;
+pub mod resource_node;
 pub mod resource_table;
 
 pub use pass_node::*;
 pub use render_context::*;
 pub use resource::*;
+pub use resource_node::*;
 pub use resource_table::*;
---
+++ src/frame_graph/resource_node.rs
@@ -0,0 +1,37 @@
+use std::marker::PhantomData;
+
+use crate::gfx_base::TypeHandle;
+
+use super::{PassNode, VirtualResource};
+
+pub struct ResourceNodeHandle<ResourceType> {
+    pub resource_node_handle: TypeHandle<ResourceNode>,
+    pub resource_handle: TypeHandle<VirtualResource>,
+    _marker: PhantomData<ResourceType>,
+}
+
+pub struct ResourceNode {
+    ///资源索引
+    pub resource_handle: TypeHandle<VirtualResource>,
+    ///自身索引
+    pub handle: TypeHandle<ResourceNode>,
+    /// 资源版本
+    pub version: u32,
+    /// 当前写入此资源节点的渲染节点
+    pub pass_node_writer: Option<TypeHandle<PassNode>>,
+}
+
+impl ResourceNode {
+    pub fn new(
+        handle: TypeHandle<ResourceNode>,
+        resource_handle: TypeHandle<VirtualResource>,
+        version: u32,
+    ) -> Self {
+        ResourceNode {
+            handle,
+            version,
+            pass_node_writer: None,
+            resource_handle,
+        }
+    }
+}
